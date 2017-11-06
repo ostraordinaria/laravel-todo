06ab79b889b9945cdeccbcc3a8a7e74a26b8ecf4
Osbert <osbert1996@gmail.com>
2017-11-07 02:30:48 +0700

    add more todo
    view, controller, route for todo added.

--- todo/app/Http/Controllers/TodoController.php
+++ todo/app/Http/Controllers/TodoController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+    	$todos = Todo::all();
+    	return view('todos')->with('todos', $todos);
+    }
 }
